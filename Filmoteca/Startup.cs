f25ddb55d09c86cdb8927789d7bac9f3929b450c
janduymonroe <janduymonroe@gmail.com.br>
2021-11-27 23:59:48 -0300

    add cloud features

--- Filmoteca/Startup.cs
+++ Filmoteca/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace Filmoteca
@@ -47,12 +46,10 @@ namespace Filmoteca
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Filmoteca v1"));
-            }
+           
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Filmoteca v1"));
+            
 
             app.UseHttpsRedirection();
 
